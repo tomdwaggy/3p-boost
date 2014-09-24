e5aee65118709517392c317e853b292be686d06b
Nat Goodspeed <nat@lindenlab.com>
2014-09-24 11:01:20 -0400

    Disable user.hpp BOOST_ALL_NO_LIB because already set in Jamroot and Xcode 5+ spams the build log with hundreds of warnings about the duplicate macro definition.

--- boost/boost/config/user.hpp
+++ boost/boost/config/user.hpp
@@ -107,7 +107,10 @@
 // build variant will be automatically selected and linked against, 
 // simply by the act of including one of that library's headers.  
 // This macro turns that feature off.
-#define BOOST_ALL_NO_LIB
+// However, DO NOT #define it here, as it's already specified in Jamroot
+// (project boost stanza) -- and some compilers feel obligated to produce a
+// warning for every compile that directly or indirectly #includes this file.
+// #define BOOST_ALL_NO_LIB
  
 // BOOST_WHATEVER_NO_LIB: Tells the config system not to automatically 
 // select which library to link against for library "whatever", 
@@ -118,6 +121,3 @@
 // by the act of including one of that library's headers.  This macro turns 
 // that feature off.
 // #define BOOST_WHATEVER_NO_LIB
- 
-
-
