096cf1015e9e31fbba7e1d3038cefcbb7f38dbdb
Drake Arconis <drake@alchemyviewer.org>
2015-01-01 17:59:29 -0500

    Attempt to silence macro redefinition warning about BOOST_ALL_NO_LIB

--- boost/boost/config/user.hpp
+++ boost/boost/config/user.hpp
@@ -107,7 +107,9 @@
 // build variant will be automatically selected and linked against, 
 // simply by the act of including one of that library's headers.  
 // This macro turns that feature off.
+#ifndef BOOST_ALL_NO_LIB
 #define BOOST_ALL_NO_LIB
+#endif
  
 // BOOST_WHATEVER_NO_LIB: Tells the config system not to automatically 
 // select which library to link against for library "whatever", 
